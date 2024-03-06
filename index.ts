3f0796755cb065def12e1d4d6f6ccbbd5fe6eba9
kevinka999 <kevinka999@gmail.com>
2024-03-06 17:14:25 -0300

    connecting mongodb and creating login route

--- index.ts
+++ index.ts
@@ -1,15 +1,64 @@
+import "dotenv/config"
 import express from "express"
+import { Db, MongoClient } from "mongodb"
 
-const app = express()
-const port = 3000
+let client: MongoClient
+let db: Db
 
-app.use(express.json())
+bootstrap()
 
-app.post("/login", (req, res) => {
-  console.log(req.body)
-  res.send("Hello World!")
-})
+async function bootstrap() {
+  await configure()
 
-app.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${port}/`)
-})
+  const app = express()
+
+  app.use(express.json())
+
+  app.post("/login", async (req, res) => {
+    const dogIdentifier = req.body.dogIndetifier
+    if (!dogIdentifier) return res.status(400).send()
+
+    const dog = await db
+      .collection("dog")
+      .findOne({ identifier: dogIdentifier })
+    if (!dog) {
+      const { insertedId } = await db
+        .collection("dog")
+        .insertOne({ identifier: dogIdentifier })
+
+      return res.send({ _id: insertedId })
+    }
+
+    return res.send({
+      _id: dog._id,
+    })
+  })
+
+  app.listen(3000, () => {
+    console.log("Server running at http://127.0.0.1:3000")
+  })
+
+  process.on("SIGINT", async () => await disconnectMongo())
+  process.on("beforeExit", async () => await disconnectMongo())
+
+  async function disconnectMongo() {
+    if (!client) return
+    console.log("Closing mongodb connection..")
+    await client.close()
+  }
+}
+
+async function configure() {
+  const uri = process.env.MONGO_URI || ""
+  const dbName = process.env.CLUSTER_NAME || ""
+
+  client = new MongoClient(uri)
+  await client.connect()
+
+  db = client.db(dbName)
+  await createIndexes(db)
+
+  async function createIndexes(db: Db) {
+    await db.collection("dogs").createIndex({ dogIndetifier: 1 })
+  }
+}
