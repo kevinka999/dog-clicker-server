8c44d4deace307140fadc5ecc83500b59a2c4f2c
kevinka999 <kevinka999@gmail.com>
2024-03-06 11:29:05 -0300

    moving to express

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
-import { createServer } from "http"
+import express from "express"
 
-const hostname = "127.0.0.1"
+const app = express()
 const port = 3000
 
-const server = createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader("Content-Type", "text/plain")
-  res.end("Hello, World!\n")
+app.use(express.json())
+
+app.post("/login", (req, res) => {
+  console.log(req.body)
+  res.send("Hello World!")
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+app.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}/`)
 })
